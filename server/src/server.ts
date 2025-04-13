fdaca5ccf25d88c341d2f1ff2982b0d3a5ca9a9b
Ajthompson88 <aj.thompson8888@gmail.com>
2025-04-13 00:07:22 -0400

    corrections and updates for additions to the project

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes/index.js';
+import weatherRoutes from './routes/api/weatherRoutes.js';
 
 import dotenv from 'dotenv';
 
@@ -13,6 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Mount the modular routers
 app.use('/api', routes);
+
+// Mount the weather route
+app.use('/api/weather', weatherRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

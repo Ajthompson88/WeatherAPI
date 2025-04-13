ac0322e9c96bd4ee8a0594be94b8a7053eada5ac
Ajthompson88 <aj.thompson8888@gmail.com>
2025-04-13 01:04:20 -0400

    changes to fix render issues hopefully

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,25 @@
 import express from 'express';
 import routes from './routes/index.js';
 import weatherRoutes from './routes/api/weatherRoutes.js';
+import path from 'path';
 
 import dotenv from 'dotenv';
 
 dotenv.config();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // Middleware to parse JSON and URL-encoded bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Serve static files from the "client/dist" directory
+app.use(express.static(path.join(__dirname, '../../client/dist')));
+
+// Root route to serve the frontend
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+});
 
 // Mount the modular routers
 app.use('/api', routes);

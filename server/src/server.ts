843a10c45a5f601754314e741078c434318dcedb
Andrew Thompson <Aj.thompson8888@gmail.com>
2025-04-13 01:13:12 -0400

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import path from 'path';
 import dotenv from 'dotenv';
 
 dotenv.config();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 
 // Middleware to parse JSON and URL-encoded bodies
